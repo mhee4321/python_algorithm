01e429c21eb4f1c5ad9a7bf87732a6976b9e1a07
mhee4321 <nannanru@gmail.com>
2021-01-20 22:45:53 +0900

    add dynamic programming

--- thisIsCodingTest/BinarySearch/makeDdeok.py
+++ thisIsCodingTest/BinarySearch/makeDdeok.py
@@ -7,14 +7,18 @@ end = max(array)
 
 result = 0
 while(start <= end):
-    total = 0
+    sum = 0
+    # 몫
     mid = (start+end) // 2
     for x in array:
         if x > mid:
-            total += x - mid
-    if total < m:
-        end = mid -1
+            sum += x - mid
+    # 떡의 양이 부족한 경우 더 많이 자르기(왼쪽록 부분 탐색)
+    if sum < m:
+        end = mid - 1
+    # 떡의 양이 충분한 경우 덜 자르기(오른쪽 부분 탐색)
     else:
+        # 최대한 덜 잘랐을 때가 정답이므로, 여기에서 result 기
         result = mid
         start = mid + 1
 
