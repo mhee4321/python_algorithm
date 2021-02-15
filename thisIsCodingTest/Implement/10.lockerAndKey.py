44206305e43f115d4ea61ed0da726e63fd092ba8
mhee4321 <nannanru@gmail.com>
2021-02-15 21:41:40 +0900

    add ThisIsCT Implement 10.lockerAndKey.py(my mistake)

--- thisIsCodingTest/Implement/10.lockerAndKey.py
+++ thisIsCodingTest/Implement/10.lockerAndKey.py
@@ -1,22 +1,48 @@
-def solutions(s):
-    answer = len(s)
-    # 1개 단위(step)부터 압축 단위를 늘려가며 확인
-    for step in range(1, len(s) // 2+1):
-        compressed = ""
-        prev = s[0:step] # 앞에서부터 step만큼의 문자열 추출
-        count = 1
-        # 단위(step) 크기만큼 증가시키며 이전 문자열과 비교
-        for j in range(step, len(s), step):
-            # 이전 상태와 동일하다면 압축 횟수(count) 증가
-            if prev == s[j:j + step]:
-                count += 1
-            # 다른 문자열이 나왔다면(더 이상 압축하지 못하는 경우라면)
-            else:
-                compressed += str(count) + prev if count >= 2 else prev
-                prev = s[j:j + step] # 다시 상태 초기화
-                count = 1
-        # 남아 있는 문자열에 대해서 처리
-        compressed += str(count) + prev if count >= 2 else prev
-        # 만들어지는 압축 문자열이 가장 짧은 것이 정답
-        answer = min(answer, len(compressed))
-    return answer
+# https://programmers.co.kr/learn/courses/30/lessons/60059
+
+# 2차원 리스트 90도 회전
+def rotate_a_matrix_by_90_degree(a):
+    n = len(a) # 행 길이 계산
+    m = len(a[0]) # 열 길이 계산
+    result = [[0] * n for _ in range(m)] # 결과 리스트
+    for i in range(n):
+        for j in range(m):
+            result[j][n-i-1] = a[i][j]
+    return result
+
+# 자물쇠의 중간 부분이 모두 1인지 확인
+def check(new_lock):
+    lock_length = len(new_lock) // 3
+    for i in range(lock_length, lock_length * 2):
+        for j in range(lock_length, lock_length * 2):
+            if new_lock[i][j] != 1:
+                return False
+    return True
+
+def solution(key, lock):
+    n = len(lock)
+    m = len(key)
+    # 자물쇠의 크기를 기존의 3배로 변환
+    new_lock = [[0] * (n*3) for _ in range(n * 3)]
+    # 새로운 자물쇠의 중앙 부분에 기존의 자물쇠 넣기
+    for i in range(n):
+        for j in range(n):
+            new_lock[i + n][j + n] = lock[i][j]
+
+    # 4가지 방향에 대해서 확인
+    for rotation in range(4):
+        key = rotate_a_matrix_by_90_degree(key) # 열쇠 회전
+        for x in range(n * 2):
+            for y in range(n * 2):
+                # 자물쇠에 열쇠를 끼워 넣기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                # 새로운 자물쇠에 열쇠가 정확히 들어맞는지 검사
+                if check(new_lock) == True:
+                    return True
+                # 자물쇠에서 열쇠를 다시 빼기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] -= key[i][j]
+    return False
