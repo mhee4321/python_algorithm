00f1ff2e4cca25f4d967cc2aadeb9549c448179c
mhee4321 <nannanru@gmail.com>
2021-02-23 22:49:44 +0900

    add ThisIsCT DFSBFS 15.specialDistanceCity.py(update)

--- thisIsCodingTest/DFSBFS/15.specialDistanceCity.py
+++ thisIsCodingTest/DFSBFS/15.specialDistanceCity.py
@@ -1,27 +1,37 @@
+# https://www.acmicpc.net/problem/18352
+# BFS, O(N+M)
 from collections import deque
 
 n, m, k, x = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 
+# 모든 도로 정보 입력받기
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
-    distance = [-1] * (n+1)
-    distance[x] = 0
 
-    q= deque([x])
-    while q:
-        now = q.popleft()
-        for next_node in graph[now]:
-            if distance[next_node] == -1:
-                distance[next_node] = distance[now] + 1
-                q.append(next_node)
+# 모든 도시에 대한 최단 거리 초기화
+distance = [-1] * (n+1)
+distance[x] = 0 # 출발 도시까지의 거리는 0으로 설정
 
-    check = False
-    for i in range(1, n+1):
-        if distance[i] == k:
-            print(i)
-            check = True
+# 너비 우선 탐색(BFS) 수행
+q = deque([x])
+while q:
+    now = q.popleft()
+    # 현재 도시에서 이동할 수 있는 모든 도시를 확인
+    for next_node in graph[now]:
+        # 아직 방문하지 않은 도시라면
+        if distance[next_node] == -1:
+            # 최단 거리 갱신
+            distance[next_node] = distance[now] + 1
+            q.append(next_node)
 
-    if check == False:
-        print(-1)
+# 최단 거리가 K인 모든 도시의 번호를 오름차순으로 출력
+check = False
+for i in range(1, n+1):
+    if distance[i] == k:
+        print(i)
+        check = True
+# 최단 거리가 K인 도시가 없다면, -1 출력
+if check == False:
+    print(-1)
