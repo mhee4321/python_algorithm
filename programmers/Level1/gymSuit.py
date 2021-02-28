1b88a44d9016a330a2aa72de2fbb1d0614e58f10
mhee4321 <nannanru@gmail.com>
2021-02-28 11:06:22 +0900

    add programmers Level1 gymSuit.py

---
+++ programmers/Level1/gymSuit.py
@@ -0,0 +1,28 @@
+def solution(n, lost, reserve):
+    # 중복제거
+    set_reserve = set(reserve) - set(lost)
+    set_lost = set(lost) - set(reserve)
+    for i in set_reserve:
+        if i-1 in set_lost:
+            set_lost.remove(i-1)
+        elif i+1 in set_lost:
+            set_lost.remove(i+1)
+    return n-len(set_lost)
+
+
+# def solution(n, lost, reserve):
+#     _reserve = [r for r in reserve if r not in lost]
+#     _lost = [l for l in lost if l not in reserve]
+#     for r in _reserve:
+#         f = r - 1
+#         b = r + 1
+#         if f in _lost:
+#             _lost.remove(f)
+#         elif b in _lost:
+#             _lost.remove(b)
+#     return n - len(_lost)
+
+n = 5
+lost = [2, 4]
+reserve = [1, 3, 5]
+print(solution(n, lost, reserve))
