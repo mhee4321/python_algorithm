c1364acd7b7969419fd63142a54dfefa7b99bd3f
mhee4321 <nannanru@gmail.com>
2021-03-03 18:43:15 +0900

    add programmers Level1 최대공약수최소공배수

--- programmers/Level1/greatestCommonFactor.py
+++ programmers/Level1/greatestCommonFactor.py
@@ -1,3 +1,25 @@
 def solution(n, m):
-    if n < m:
-        i, j =divmod(m, n)
+    for i in range(1, n+1):
+        if n % i == 0:
+            if m % i == 0:
+                gcd = i
+    if gcd == 1:
+        lcm = n*m
+    else:
+        lcm = gcd * (n // gcd) * (m // gcd)
+    return [gcd, lcm]
+
+def gcdlcm(a, b):
+    c, d = max(a, b), min(a, b)
+    t = 1
+    while t > 0:
+        t = c % d
+        c, d = d, t
+    answer = [c, int(a*b/c)]
+
+    return answer
+
+n = 2
+m= 5
+print(solution(n, m))
+
