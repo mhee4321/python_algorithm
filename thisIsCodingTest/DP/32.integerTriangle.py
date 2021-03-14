ad24eee77f4263df288cbdb3c03a91c4452fbdf9
mhee4321 <nannanru@gmail.com>
2021-03-14 16:58:28 +0900

    thisIsCodingTest reconstruction(dp)

--- thisIsCodingTest/DP/32.integerTriangle.py
+++ thisIsCodingTest/DP/32.integerTriangle.py
@@ -1,23 +1,21 @@
-# https://www.acmicpc.net/problem/1932
 n = int(input())
 dp = []
 
 for _ in range(n):
     dp.append(list(map(int, input().split())))
 
+
 for i in range(1, n):
-    for j in range(i + 1):
-        # 왼쪽 위에서 내려오는 경우
-        if j == 0:
-            up_left = 0
+    for j in range(i+1):
+        if j==0:
+            top_left = 0
         else:
-            up_left = dp[i-1][j-1]
-        # 바로 위에서 내려오는 경우
-        if j == i:
-            up = 0
+            top_left = dp[i-1][j-1]
+        if j==i:
+            top = 0
         else:
-            up = dp[i-1][j]
-        # 최대 합을 저장
-        dp[i][j] = dp[i][j] + max(up_left, up)
+            top = dp[i-1][j]
+
+        dp[i][j] = dp[i][j] + max(top, top_left)
 
 print(max(dp[n-1]))
