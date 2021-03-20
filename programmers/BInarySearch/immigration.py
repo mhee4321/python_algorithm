07c181b2c63fa0998c2f2ffd8b4ab09456e3e6a2
mhee4321 <nannanru@gmail.com>
2021-03-21 08:40:14 +0900

    programmers heap moreSpicier.py

--- programmers/BInarySearch/immigration.py
+++ programmers/BInarySearch/immigration.py
@@ -1,26 +1,23 @@
 def solution(n, times):
-    answer = 0
-
-    leng = len(times)
+    right = min(times) * n
     left = 1
-    right = (leng+1) * max(times) # 최대 범위
 
-    while left <= right:
-        mid = (left + right) // 2
-
-        count = 0
-        for time in times:
-            count += mid // time
-            # 심사 인원수를 넘으면 다음 단계
-            if count >= n: break
-
-        # n명을 심사 할 수 있는 경우
-        # 한 심사관에게 주어진 시간을 줄여본다.
-        if count >= n:
-            answer = mid
-            right = mid - 1
-        # 없는 경우
-        elif count < n:
+    answer = 0
+    while(left <= right):
+        mid = (right + left) // 2
+        temp = n
+        for i in times:
+            temp -= mid//i
+            if temp <= 0:
+                answer = mid
+                right = mid - 1
+                break
+        if temp>0:
             left = mid + 1
-
     return answer
+
+
+
+n = 6
+times = [7, 10]
+print(solution(n, times))
