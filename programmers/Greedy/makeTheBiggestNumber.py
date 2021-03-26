5d66af75aac02ef25a7834b461b79ed584ad5aaf
mhee4321 <nannanru@gmail.com>
2021-03-26 13:40:53 +0900

    programmers Greedy makeTheBiggestNumber.py

---
+++ programmers/Greedy/makeTheBiggestNumber.py
@@ -0,0 +1,12 @@
+def solution(number, k):
+    st = []
+    for i in range(len(number)):
+        while st and k > 0 and st[-1] < number[i]:
+            st.pop()
+            k -= 1
+        st.append(number[i])
+    return ''.join(st[:len(st) - k])
+
+number = "1924"
+k = 2
+print(solution(number, k))
