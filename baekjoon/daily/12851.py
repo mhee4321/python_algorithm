b07a3950d15b86940649cc21cbd044ba0e8253be
mhee4321 <nannanru@gmail.com>
2021-04-02 14:09:27 +0900

    baekjoon daily dp

--- baekjoon/daily/12851.py
+++ baekjoon/daily/12851.py
@@ -5,20 +5,20 @@ import sys
 input = lambda: sys.stdin.readline().strip()
 
 def bfs(n):
-    de=deque()
-    de.append(n)
+    dq = deque()
+    dq.append(n)
     ch[n][0]=0
     ch[n][1]=1
 
-    while de:
-        x=de.popleft()
+    while dq:
+        x = dq.popleft()
 
         for i in (x+1, x-1, x*2):
-            if 0 <= i < 100001:
+            if 0 <= i < 1000001:
                 if ch[i][0] == -1: # 처음 방문하는 경우
                     ch[i][0] = ch[x][0]+1
                     ch[i][1] = ch[x][1]
-                    de.append(i)
+                    dq.append(i)
 
                 elif ch[i][0] == ch[x][0]+1: # 한번 이상 방문한 경우
                     ch[i][1] += ch[x][1] # 방법 더하기
