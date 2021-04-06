2255c4d0279f92572fc06786dd50f46e22e43e6c
mhee4321 <nannanru@gmail.com>
2021-04-07 08:07:44 +0900

    programmers daily

---
+++ programmers/daily/nforX.py
@@ -0,0 +1,11 @@
+def solution(x, n):
+    answer = []
+    for i in range(1, n + 1):
+        answer.append(x * i)
+    return answer
+
+# return [x*i for i in range(1, n+1)]
+
+x = -4
+n = 2
+print(solution(x, n))
